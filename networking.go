d7f81eb87b54064305361f984b4a7c8651d1a8e7
sshehrozali <sshehroza13@gmail.com>
2026-01-16 03:35:59 +0500

    extracted srcip and dstport from ethernet packet

--- networking.go
+++ networking.go
@@ -23,6 +23,13 @@ func main() {
 			for {
 				data, _, _ := handle.ReadPacketData()
 				fmt.Printf("packets: %s", hex.Dump(data))
+				
+				sourceIP := data[26:30]
+				dstPort := uint16(data[36])<<8 | uint16(data[37])
+
+				fmt.Printf("Source IP: %d.%d.%d.%d:%d", sourceIP[0], sourceIP[1], sourceIP[2], sourceIP[3], dstPort)
+
+				// fmt.Printf("Payload: %s", string(data[54:]))
 			}
 		}
 	}
