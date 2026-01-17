b81b0e52cc2e49bbd4b582d168637e7092df2ca6
sshehrozali <sshehroza13@gmail.com>
2026-01-18 04:22:04 +0500

    extracted TCP sequence no and calculated payload size

--- networking.go
+++ networking.go
@@ -22,40 +22,52 @@ func main() {
 
 			for {
 				data, _, _ := handle.ReadPacketData()
-				// fmt.Printf("packets: %s", hex.Dump(data))
 
+				// IP and TCP header sizes
 				ipHeaderSize := int(data[14]&0x0F) * 4
 				fmt.Printf("\nIP header size: %d", ipHeaderSize)
 
 				tcpHeaderSize := int(data[46]>>4) * 4
 				fmt.Printf("\nTCP header size: %d", tcpHeaderSize)
 
+				// IPs
 				srcIPBytes := data[26:30]
 				dstIPBytes := data[30:34]
 
 				srcIP := fmt.Sprintf("%d.%d.%d.%d", srcIPBytes[0], srcIPBytes[1], srcIPBytes[2], srcIPBytes[3])
 				dstIP := fmt.Sprintf("%d.%d.%d.%d", dstIPBytes[0], dstIPBytes[1], dstIPBytes[2], dstIPBytes[3])
 
+				// Ports
 				srcPort := uint16(data[34])<<8 | uint16(data[35])
 				dstPort := uint16(data[36])<<8 | uint16(data[37])
 
 				fmt.Printf("\nSource IP: %s:%d", srcIP, srcPort)
 				fmt.Printf("\nDestination IP: %s:%d", dstIP, dstPort)
 
+				// BinderID to stich packets
 				binderId := fmt.Sprintf("%s-%d-%s-%d", srcIP, srcPort, dstIP, dstPort)
 				fmt.Printf("\nBinder ID: %s", binderId)
 
+				// TCP sequence no
+				tcpSequenceNoBytes := data[38:42]
+				tcpSequenceNo := uint32(tcpSequenceNoBytes[0]) << 24 | uint32(tcpSequenceNoBytes[1]) << 16 | uint32(tcpSequenceNoBytes[2]) << 8 | uint32(tcpSequenceNoBytes[3]) << 0
+				fmt.Printf("\nSequence No: %d", tcpSequenceNo)
+
+				// Payload size
+				payloadSize := len(data) - ipHeaderSize - tcpHeaderSize
+				fmt.Printf("Payload size: %d", payloadSize)
+
+				// Packet type
 				packetType := ""
-				
 				if (dstPort == 80) {
 					packetType = "[REQUEST]"
 				} else if (srcPort == 80) {
 					packetType = "[RESPONSE]"
 				}
 
+				// TCP handshake
 				flags := data[47]
 				info := ""
-
 				if (flags & 0x02) != 0 {
 					info += "[SYN] "
 				}
